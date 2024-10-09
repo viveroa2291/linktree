3487e5a118b34cc36862120fa48961aa99381f69
viveroa2291 <viveroa2291@uwec.edu>
2024-10-08 22:54:10 -0500

    Some fixes

--- src/instagramScrape.js
+++ src/instagramScrape.js
@@ -8,7 +8,7 @@ async function scrapeInstaFollowers() {
 
     await page.goto('https://www.instagram.com/adxadan/');
 
-    await page.waitForSelector('.html-span.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x1hl2dhg.x16tdsg8.x1vvkbs');
+    await page.waitForSelector('.html-span.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x1hl2dhg.x16tdsg8.x1vvkbs', {timeout: 90000});
     
     // const followerCount = await page.$eval('.html-span.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x1hl2dhg.x16tdsg8.x1vvkbs', element => element.textContent);
 
--- src/twitterScrape.js
+++ src/twitterScrape.js
@@ -3,19 +3,30 @@ const puppeteer = require('puppeteer');
 async function scrapeTwitterFollowers() {
     const browser = await puppeteer.launch({
         headless: "new",
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        // args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
     const page = await browser.newPage();
 
-    await page.goto('https://x.com/adan_vivero', { waitUntil: 'networkidle2'});
+    await page.goto('https://x.com/adan_vivero' , { waitUntil: 'networkidle0'});
 
-    // await page.waitForSelector('.css-1jxf684.r-bcqeeo.r-1ttztb7.r-qvutc0.r-poiln3', {timeout: 60000});
+    await page.waitForSelector('span.css-1jxf684.r-bcqeeo.r-1ttztb7.r-qvutc0.r-poiln3', {timeout: 90000});
+
+    const elements = await page.$$('span.css-1jxf684.r-bcqeeo.r-1ttztb7.r-qvutc0.r-poiln3');
+
+    if(elements.length < 17 ) {
+        throw new Error('The 17th element with the specified class was not found');
+    }
+    const targetElement = elements[16];
+
+    const followerCount = await page.evaluate(el => el.textContent, targetElement);
+
+    /*
     await page.waitForFunction(
         `document.querySelector('.css-1jxf684.r-bcqeeo.r-1ttztb7.r-qvutc0.r-poiln3') && window.getComputedStyle(document.querySelector('.css-1jxf684.r-bcqeeo.r-1ttztb7.r-qvutc0.r-poiln3')).display !== 'none'`,
         { timeout: 60000 } // Increased timeout to 60 seconds
     );
-
-    const followerCount = await page.$eval('.css-1jxf684.r-bcqeeo.r-1ttztb7.r-qvutc0.r-poiln3', element => element.textContent);
+    */
+   // const followerCount = await page.$eval('.css-1jxf684.r-bcqeeo.r-1ttztb7.r-qvutc0.r-poiln3', element => element.textContent);
     console.log('This is the Twitter follower count: ' + followerCount);
 
     await browser.close();
